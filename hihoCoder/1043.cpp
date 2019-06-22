d210cee59a55bf28fe6ee77d0e2a6d9b423bb1b0
x1314aq <x1314aq@gmail.com>
2019-06-23 00:10:56 +0800

    add hihoCoder problem 1043

---
+++ hihoCoder/1043.cpp
@@ -0,0 +1,28 @@
+/**
+ * hihoCoder 1043  完全背包
+ * DP
+ */
+
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int need[505];
+int value[505];
+int best[100005];
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    for(int i = 1; i <= n; i++)
+        cin >> need[i] >> value[i];
+
+    for(int i = 1; i <= n; i++)
+        for(int j = need[i]; j <= m; j++)
+            best[j] = max(best[j], best[j - need[i]] + value[i]);
+    
+    cout << best[m] << endl;
+    return 0;
+}
