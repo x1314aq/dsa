08f6e2c66dd6b8d4c4b43fbfc0b78983d6f24f9e
x1314aq <x1314aq@gmail.com>
2019-06-23 13:44:37 +0800

    add LeetCode problem 198 && 213

---
+++ house-robber-1.cpp
@@ -0,0 +1,32 @@
+/**
+ * Leetcode 198 House Robber
+ * DP
+ */
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int rob(vector<int> &nums) {
+        size_t n = nums.size();
+        if(n == 0) return 0;
+        if(n == 1) return nums[0];
+
+        int a, b, c;
+        a = nums[0];
+        b = max(a, nums[1]);
+        c = b;
+
+        for(size_t i = 2; i < n; i++) {
+            c = max(b, a + nums[i]);
+            a = b;
+            b = c;
+        }
+
+        return c;
+    }
+};
---
+++ house-robber-2.cpp
@@ -0,0 +1,33 @@
+/**
+ * Leetcode 213 House Robber 2
+ * DP
+ */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int rob(vector<int> &nums) {
+        size_t n = nums.size();
+        if(n == 0) return 0;
+        if(n == 1) return nums[0];
+        
+        int a = one(nums, 1, n);
+        int b = one(nums, 0, n - 1);
+        return max(a, b);
+    }
+
+    int one(vector<int> &nums, int l, int r) {
+        int a = 0, b = 0, c;
+        for(int i = l; i < r; i++) {
+            c = max(b, a + nums[i]);
+            a = b;
+            b = c;
+        }
+
+        return b;
+    }
+};
