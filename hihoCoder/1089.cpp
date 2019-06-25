9e1147057aa9b4dc94ba6eaee34e2dfa06df901f
x1314aq <x1314aq@gmail.com>
2019-06-26 00:08:26 +0800

    add hihoCoder problem 1089

---
+++ hihoCoder/1089.cpp
@@ -0,0 +1,45 @@
+/**
+ * hihoCoder 1089  最短路径二
+ * Floyd Algorithm
+ */
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int g[105][105];
+int dist[105][105];
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    for(int i = 0; i <= n; i++)
+        for(int j = 0; j <= n; j++)
+            g[i][j] = 10000;
+    for(int i = 1; i <= m; i++) {
+        int u, v, d;
+        cin >> u >> v >> d;
+        g[u][v] = g[v][u] = min(g[u][v], d);
+    }
+
+    for(int i = 1; i <= n; i++)
+        for(int j = 1; j <= n; j++)
+            dist[i][j] = (i != j ? g[i][j] : 0);
+
+    for(int k = 1; k <= n; k++)
+        for(int i = 1; i <= n; i++)
+            for(int j = 1; j <= n; j++)
+                if(dist[i][j] > dist[i][k] + dist[k][j]) {
+                    dist[i][j] = dist[i][k] + dist[k][j];
+                }
+
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n; j++)
+            cout << dist[i][j]<< " ";
+        cout << endl;
+    }
+    return 0;
+}
