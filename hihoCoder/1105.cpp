64efede1f4c8fe2456f39cf0196abc18fbd5c66c
x1314aq <x1314aq@gmail.com>
2019-06-30 10:59:34 +0800

    add hihoCoder problem 1105

---
+++ hihoCoder/1105.cpp
@@ -0,0 +1,36 @@
+/**
+ * hihoCoder 1105  题外话 堆
+ * priority_queue
+ */
+
+#include <functional>
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n;
+    vector<int> ans;
+    priority_queue<int> pq;
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        int num;
+        char c;
+        cin >> c;
+        if(c == 'A') {
+            cin >> num;
+            pq.push(num);
+        }
+        else {
+            ans.push_back(pq.top());
+            pq.pop();
+        }
+    }
+
+    for(size_t i = 0; i < ans.size(); i++)
+        cout << ans[i] << endl;
+    return 0;
+}
