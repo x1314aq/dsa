2bc57dbf4fbd21213e82c866cf8a5522b5befd3b
x1314aq <x1314aq@gmail.com>
2019-09-08 22:47:25 +0800

    add hihoCoder problem 1977

---
+++ hihoCoder/1977.cpp
@@ -0,0 +1,31 @@
+/**
+ * hihoCoder 1977  小Hi与魔法
+ */
+
+#include <vector>
+#include <cstdio>
+#include <cstdlib>
+
+using namespace std;
+
+int hs[1000001];
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    for(int i = 0; i < n; i++) {
+        int tmp;
+        scanf("%d", &tmp);
+        hs[tmp]++;
+    }
+
+    int ans = 0;
+    for(int i = 1; i <= 1000000; i++) {
+        if(hs[i] == 0)
+            break;
+        ans += hs[i];
+    }
+
+    printf("%d\n", ans);
+    return 0;
+}
