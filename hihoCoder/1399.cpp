a1637e4695b921a07d23a2b25e18c5145098b596
x1314aq <x1314aq@gmail.com>
2019-09-14 00:27:14 +0800

    add hihoCoder problem 1399

---
+++ hihoCoder/1399.cpp
@@ -0,0 +1,34 @@
+/**
+ * hihoCoder 1399  shortening Sequence
+ */
+
+#include <iostream>
+#include <vector>
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+    int n;
+
+    vector<int> v;
+    scanf("%d", &n);
+    for(int i = 1; i <= n; i++) {
+        int tmp;
+        scanf("%d", &tmp);
+
+        if(!v.size()) {
+            v.push_back(tmp);
+            continue;
+        }
+
+        int a = v.back();
+        if((a + tmp) & 1)
+            v.pop_back();
+        else
+            v.push_back(tmp);
+    }
+
+    printf("%zu\n", v.size());
+    return 0;
+}
