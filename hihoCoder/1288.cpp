4e173c93b19d554dce167689a94d18f1cc49cf81
x1314aq <x1314aq@gmail.com>
2019-09-13 15:49:21 +0800

    add hihoCoder problem 1288

---
+++ hihoCoder/1288.cpp
@@ -0,0 +1,54 @@
+/**
+ * hihoCoder 1288  font size
+ */
+
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <vector>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+
+using namespace std;
+
+int a[1005];
+int n, p, w, h;
+
+bool check(int x) {
+    int res = 0;
+    int tmp = w / x;
+    for(int i = 1; i <= n; i++)
+        res += ceil(1.0 * a[i] / tmp);
+    tmp = ceil(1.0 * res / (h / x));
+    return tmp > p;
+}
+
+int bsearch(int l, int r) {
+    int mid;
+    while(l < r) {
+        mid = (l + r) / 2;
+        if(check(mid))
+            r = mid;
+        else
+            l = mid + 1;
+    }
+    return l - 1;
+}
+
+int main() {
+    int m;
+    scanf("%d", &m);
+    vector<int> ans;
+    for(int i = 1; i <= m; i++) {
+        scanf("%d %d %d %d", &n, &p, &w, &h);
+        for(int j = 1; j <= n; j++)
+            scanf("%d", &a[j]);
+
+        int lim = min(w, h);
+        ans.push_back(bsearch(1, lim + 1));
+    }
+    for(int i = 0; i < m; i++)
+        printf("%d\n", ans[i]);
+    return 0;
+}
