042c7eec243043806a218b9696143d8e21810985
x1314aq <x1314aq@gmail.com>
2019-09-24 23:40:34 +0800

    add 51nod 1046

---
+++ 51nod/1046.cpp
@@ -0,0 +1,24 @@
+//
+// 1046 A^B Mod C
+//
+
+#include <bits/stdc++.h>
+
+using namespace std;
+using  ull = unsigned long long;
+
+int main() {
+    ull a, b, c;
+    cin >> a >> b >> c;
+
+    ull res = 1;
+    while(b) {
+        if(b & 1)
+            res = (a * res) % c;
+        a = (a * a) % c;
+        b >>= 1;
+    }
+
+    cout << res % c << endl;
+    return 0;
+}
