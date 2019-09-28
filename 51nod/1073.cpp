2cb834f2575b4b196f91537c3533f4c96307bc92
x1314aq <x1314aq@gmail.com>
2019-09-28 14:50:34 +0800

    add 51nod 1073

---
+++ 51nod/1073.cpp
@@ -0,0 +1,17 @@
+//
+// 1073 约瑟夫环
+//
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+    int res = 0;
+    for(int i = 2; i <= n; i++)
+        res = (res + k) % i;
+    cout << res + 1 << endl;
+    return 0;
+}
