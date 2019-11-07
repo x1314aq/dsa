18d23ba0e410ad98301ad5de03c9fb4540a9bbb9
x1314aq <x1314aq@gmail.com>
2019-11-08 00:31:12 +0800

    add 51nod 1284

---
+++ 51nod/1284.cpp
@@ -0,0 +1,21 @@
+//
+// 1284 2 3 5 7的倍数
+//
+
+#include <bits/stdc++.h>
+
+using namespace std;
+using ull = unsigned long long;
+
+int main() {
+    ull n, ans;
+
+    cin >> n;
+    ans = n;
+    ans -= (n / 2 + n / 3 + n / 5 + n / 7);
+    ans += (n / 6 + n / 10 + n / 14 + n / 15 + n / 21 + n / 35);
+    ans -= (n / 30 + n/ 42 + n / 70 + n / 105);
+    ans += (n / 210);
+    cout << ans << endl;
+    return 0;
+}
