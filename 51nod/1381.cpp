fc9065fb53e3a26aa5874f881b933f6d25ad5de1
x1314aq <x1314aq@gmail.com>
2019-11-09 14:10:02 +0800

    add 51nod 1381

---
+++ 51nod/1381.cpp
@@ -0,0 +1,19 @@
+//
+// 1381 硬币游戏
+//
+
+#include <bits/stdc++.h>
+
+using namespace std;
+using ull = unsigned long long;
+
+int main() {
+    int n;
+    cin >> n;
+    while (n--) {
+        ull r;
+        cin >> r;
+        cout << 2 * r << endl;
+    }
+    return 0;
+}
