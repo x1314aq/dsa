a3e7ec18f1805ae32839ccc217a0a693f1ffe473
x1314aq <x1314aq@gmail.com>
2019-11-24 21:03:11 +0800

    add 51nod 2653

---
+++ 51nod/2653.cpp
@@ -0,0 +1,29 @@
+//
+// 2653 区间xor
+//
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int fn(int x) {
+    switch(x % 4) {
+    case 0:
+        return x;
+    case 1:
+        return 1;
+    case 2:
+        return x + 1;
+    case 3:
+        return 0;
+    }
+    return x;
+}
+
+int main() {
+    int a, b, ans;
+
+    cin >> a >> b;
+    cout << (fn(a - 1) ^ fn(b)) << endl;
+    return 0;
+}
