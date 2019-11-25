afb24ea486cea8d8c5d6120e131a06f4cec575c2
x1314aq <x1314aq@gmail.com>
2019-11-25 21:31:19 +0800

    add 51nod 2497

---
+++ 51nod/2497.cpp
@@ -0,0 +1,27 @@
+//
+// 2497 数三角形
+//
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, a[1005], ans;
+
+int main() {
+    cin >> n;
+    for(int i = 1; i <= n; i++)
+        cin >> a[i];
+
+    sort(a + 1, a + 1 + n);
+    for(int i = 1; i <= n - 1; i++) {
+        for(int j = i + 1; j <= n; j++) {
+            int k = lower_bound(a + 1, a + 1 + n, a[i] + a[j]) - a - 1;
+            if(k >= j)
+                ans += k - j;
+        }
+    }
+
+    cout << ans << endl;
+    return 0;
+}
