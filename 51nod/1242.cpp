a831d8b61aecbda7bf6eacfccebbdbf1473adde9
x1314aq <x1314aq@gmail.com>
2019-12-14 11:21:41 +0800

    update 51nod 1242

--- 51nod/1242.cpp
+++ 51nod/1242.cpp
@@ -14,22 +14,24 @@ ull a[2][2] = {
     {1, 0},
 };
 
-void matrix_multiply(ull a[2][2], ull b[2][2]) {
+void matrix_multiply(ull ans[2][2]) {
     ull c[2][2] = {
-        {a[0][0], a[0][1]},
-        {a[1][0], a[1][1]},
+        {ans[0][0], ans[0][1]},
+        {ans[1][0], ans[1][1]},
     };
-    a[0][0] = ((c[0][0] * b[0][0] % mod) + (c[0][1] * b[1][0] % mod)) % mod;
-    a[0][1] = ((c[0][0] * b[0][1] % mod) + (c[0][1] * b[1][1] % mod)) % mod;
-    a[1][0] = ((c[1][0] * b[0][0] % mod) + (c[1][1] * b[1][0] % mod)) % mod;
-    a[1][1] = ((c[1][0] * b[0][1] % mod) + (c[1][1] * b[1][1] % mod)) % mod;
+
+    ans[0][0] = ((c[0][0] * a[0][0] % mod) + (c[0][1] * a[1][0] % mod)) % mod;
+    ans[0][1] = ((c[0][0] * a[0][1] % mod) + (c[0][1] * a[1][1] % mod)) % mod;
+    ans[1][0] = ((c[1][0] * a[0][0] % mod) + (c[1][1] * a[1][0] % mod)) % mod;
+    ans[1][1] = ((c[1][0] * a[0][1] % mod) + (c[1][1] * a[1][1] % mod)) % mod;
 }
 
-void matrix_square(ull a[2][2]) {
+void matrix_square() {
     ull c[2][2] = {
         {a[0][0], a[0][1]},
         {a[1][0], a[1][1]},
     };
+
     a[0][0] = ((c[0][0] * c[0][0] % mod) + (c[0][1] * c[1][0] % mod)) % mod;
     a[0][1] = ((c[0][0] * c[0][1] % mod) + (c[0][1] * c[1][1] % mod)) % mod;
     a[1][0] = ((c[1][0] * c[0][0] % mod) + (c[1][1] * c[1][0] % mod)) % mod;
@@ -48,9 +50,9 @@ int main() {
 
     while(n) {
         if(n & 1)
-            matrix_multiply(ans, a);
+            matrix_multiply(ans);
         n >>= 1;
-        matrix_square(a);
+        matrix_square();
     }
 
     cout << ans[0][0] << endl;
