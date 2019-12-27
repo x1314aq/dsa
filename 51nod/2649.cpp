0aaa7c6286d47148d75b88f0c0886c9431323993
x1314aq <x1314aq@gmail.com>
2019-12-27 23:18:10 +0800

    add 51nod 2649

---
+++ 51nod/2649.cpp
@@ -0,0 +1,25 @@
+//
+// 2649 完全背包
+//
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int C[105], W[105];
+int n, v;
+int dp[50005];
+
+int main() {
+    cin >> n >> v;
+    for (int i = 0; i < n; i++)
+        cin >> C[i] >> W[i];
+
+    for (int i = 0; i < n; i++)
+        for (int j = C[i]; j <= v; j++)
+            dp[j] = max(dp[j], dp[j - C[i]] + W[i]);
+
+
+    cout << dp[v] << endl;
+    return 0;
+}
