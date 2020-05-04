a4ba9a4b3905f69b761c87fa8abe01866a47ac57
x1314aq <x1314aq@sina.cn>
2020-05-04 12:46:40 +0800

    add luogu 1044 & 1334

---
+++ luogu/1044.cpp
@@ -0,0 +1,25 @@
+//
+// 1044 栈
+//
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int m[20][20];
+
+int dfs(int i, int j) {
+    if (m[i][j]) return m[i][j];
+    if (i == 0) return 1;
+    if (j > 0) m[i][j] += dfs(i, j - 1);
+    m[i][j] += dfs(i - 1, j + 1);
+    return m[i][j];
+}
+
+int main() {
+    int n, ans;
+    ios::sync_with_stdio(false);
+    cin >> n;
+    cout << dfs(n, 0) << endl;
+    return 0;
+}
---
+++ luogu/1334.cpp
@@ -0,0 +1,34 @@
+//
+// 1334 瑞瑞的木板
+//
+
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+int main() {
+    priority_queue<ll, vector<ll>, greater<ll>> pq;
+    int n;
+    ll ans = 0;
+    
+    ios::sync_with_stdio(false);
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int tmp;
+        cin >> tmp;
+        pq.push(tmp);
+    }
+
+    while (pq.size() > 1) {
+        int u = pq.top();
+        pq.pop();
+        int v = pq.top();
+        pq.pop();
+        pq.push(u + v);
+        ans += (u + v);
+    }
+
+    cout << ans << endl;
+    return 0;
+}
