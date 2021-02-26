a5ec58ced10ecd6d37e94872f11e0d6a86e367c2
x1314aq <469221983@qq.com>
2021-02-26 14:45:00 +0800

    add luogu 1002 & 1147

---
+++ luogu/1002.cpp
@@ -0,0 +1,45 @@
+//
+// 1002 过河卒
+//
+
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef unsigned long long ull;
+
+int n,m,x,y;
+ull a[25][25];
+
+static inline bool die(int i, int j) {
+    if (i == x && j == y)
+        return true;
+    if (abs(i - x) == 1 && abs(j - y) == 2)
+        return true;
+    if (abs(i - x) == 2 && abs(j - y) == 1)
+        return true;
+    return false;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin >> n >> m >> x >> y;
+    if (!die(0, 0)) a[0][0] = 1;
+    for (int i = 1; i <= n; i++) {
+        if (!die(i, 0))
+            a[i][0] = a[i - 1][0];
+    }
+    for (int i = 1; i <= m; i++) {
+        if (!die(0, i))
+            a[0][i] = a[0][i - 1];
+    }
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1;j <= m; j++) {
+            a[i][j] = a[i - 1][j] + a[i][j - 1];
+            if (die(i, j))
+                a[i][j] = 0;
+        }
+    }
+
+    cout << a[n][m] << endl;
+    return 0;
+}
---
+++ luogu/1147.cpp
@@ -0,0 +1,34 @@
+//
+// 1147 连续自然数的和
+//
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+ll n;
+
+static inline ll s(ll x) {
+    return x*(x+1)/2;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin >> n;
+    for (ll i = 1; i <= n / 2; i++) {
+        ll s0 = s(i - 1);
+        ll l = i, r = n;
+        while (l < r) {
+            ll m = l + (r - l) / 2;
+            ll sm = s(m) - s0;
+            if (sm >= n) r = m;
+            else l = m + 1;
+        }
+        if (s(l) - s0 == n) {
+            cout << i << ' ' << l << endl;
+        }
+    }
+    return 0;
+}
