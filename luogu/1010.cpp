aa823ffa13cd0f7b0fd90d4578e7723bf89f3291
x1314aq <x1314aq@sina.cn>
2021-03-13 15:42:31 +0800

    add luogu 1010

---
+++ luogu/1010.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long int;
+
+string run(int x,int i=0,string s=string("")){
+	if(x==0) return string("0");
+    do {
+        if(x&1) s=(i==1?"2":"2("+run(i)+")")+(s==""?"":"+")+s;
+    } while(++i,x>>=1);
+	return s;
+}
+
+int main(){
+	int x;cin>>x;
+	cout<<run(x)<<endl;
+}
