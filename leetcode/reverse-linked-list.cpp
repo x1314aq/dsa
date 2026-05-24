7dd054b86e274d6b5a90c8777e1a760f2b431af4
x131aq <x1314aq@icloud.com>
2026-05-24 15:17:21 +0800

    add leetcode 206

---
+++ leetcode/reverse-linked-list.cpp
@@ -0,0 +1,28 @@
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode *prev = nullptr;
+        ListNode *cur = head;
+        while (cur) {
+            ListNode *next = cur->next;
+            cur->next = prev;
+            prev = cur;
+            cur = next;
+        }
+        return prev;
+    }
+};
+
+int main(int argc, char *argv[]) {
+    return 0;
+}
